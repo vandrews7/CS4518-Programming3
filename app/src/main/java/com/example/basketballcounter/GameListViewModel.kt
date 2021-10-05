ddedc3ef014ec89f3233abfada2d0bbd2fba9cf4
vandrews7 <iamveronicaandrews@outlook.com>
2021-10-05 13:54:06 -0400

    idk

--- app/src/main/java/com/example/basketballcounter/GameListViewModel.kt
+++ app/src/main/java/com/example/basketballcounter/GameListViewModel.kt
@@ -4,18 +4,22 @@ import androidx.lifecycle.ViewModel
 import java.util.*
 
 class GameListViewModel : ViewModel() {
-    val games = mutableListOf<Game>()
+//    val games = mutableListOf<Game>()
+//
+//    init {
+//        for(i in 0 until 100) {
+//            val game = Game()
+//            game.index = "Game #$i"
+//            game.scoreA = (0..100).random()
+//            game.scoreB = (0..100).random()
+//            game.teamAname = "TeamA"
+//            game.teamBname = "TeamB"
+//            game.date = Date()
+//            games += game
+//        }
+//    }
 
-    init {
-        for(i in 0 until 100) {
-            val game = Game()
-            game.index = "Game #$i"
-            game.scoreA = (0..100).random()
-            game.scoreB = (0..100).random()
-            game.teamAname = "TeamA"
-            game.teamBname = "TeamB"
-            game.date = Date()
-            games += game
-        }
-    }
+    private val gameRepository = GameRepository.get()
+    val games = gameRepository.getGames()
+
 }
