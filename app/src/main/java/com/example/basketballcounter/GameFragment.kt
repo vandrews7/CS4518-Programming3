0d8bf4849cf890573c669e2e5471728f49d76b88
Maranda117 <mmallen@wpi.edu>
2021-10-05 16:47:12 -0400

    photo buttons

--- app/src/main/java/com/example/basketballcounter/GameFragment.kt
+++ app/src/main/java/com/example/basketballcounter/GameFragment.kt
@@ -1,7 +1,11 @@
 package com.example.basketballcounter
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -9,6 +13,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.Observer
@@ -17,6 +22,7 @@ import java.util.*
 
 private const val TAG = "GameFragment"
 private const val REQUEST_CODE = 0
+private const val REQUEST_PHOTO = 2
 private const val ARG_GAME_ID = "game_id"
 
 class GameFragment: Fragment() {
@@ -27,6 +33,7 @@ class GameFragment: Fragment() {
 
     private lateinit var game: Game
     private lateinit var photoFile: File
+    private lateinit var photoUri: Uri
     private lateinit var teamAname: EditText
     private lateinit var teamBname: EditText
     private lateinit var teamAscore: TextView
@@ -159,6 +166,9 @@ class GameFragment: Fragment() {
                 game?.let {
                     this.game = game
                     photoFile = gameDetailViewModel.getPhotoFile(game)
+                    photoUri = FileProvider.getUriForFile(requireActivity(),
+                        "com.example.basketballcounter.FileProvider",
+                        photoFile)
                     updateUI()
                 }
             }
@@ -167,6 +177,62 @@ class GameFragment: Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        photoButtonA.apply {
+            val packageManager: PackageManager = requireActivity().packageManager
+            val captureImage = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val resolvedActivity: ResolveInfo? =
+                packageManager.resolveActivity(captureImage,
+                PackageManager.MATCH_DEFAULT_ONLY)
+            if (resolvedActivity == null){
+                isEnabled = false
+            }
+
+            setOnClickListener{
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+
+                val cameraActivities: List<ResolveInfo> =
+                    packageManager.queryIntentActivities(captureImage,
+                    PackageManager.MATCH_DEFAULT_ONLY)
+
+                for(cameraActivity in cameraActivities) {
+                    requireActivity().grantUriPermission(
+                        cameraActivity.activityInfo.packageName,
+                        photoUri,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                }
+
+                startActivityForResult(captureImage, REQUEST_PHOTO)
+            }
+        }
+
+        photoButtonB.apply {
+            val packageManager: PackageManager = requireActivity().packageManager
+            val captureImage = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val resolvedActivity: ResolveInfo? =
+                packageManager.resolveActivity(captureImage,
+                    PackageManager.MATCH_DEFAULT_ONLY)
+            if (resolvedActivity == null){
+                isEnabled = false
+            }
+
+            setOnClickListener{
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+
+                val cameraActivities: List<ResolveInfo> =
+                    packageManager.queryIntentActivities(captureImage,
+                        PackageManager.MATCH_DEFAULT_ONLY)
+
+                for(cameraActivity in cameraActivities) {
+                    requireActivity().grantUriPermission(
+                        cameraActivity.activityInfo.packageName,
+                        photoUri,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                }
+
+                startActivityForResult(captureImage, REQUEST_PHOTO)
+            }
+        }
 
         val teamAWatcher = object: TextWatcher{
             override fun beforeTextChanged(sequence: CharSequence?,
