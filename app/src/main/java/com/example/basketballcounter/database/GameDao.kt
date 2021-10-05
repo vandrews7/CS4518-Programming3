4a625800a659eaa3c981dd024e6c7c95b440467e
Maranda117 <mmallen@wpi.edu>
2021-10-05 12:28:17 -0400

    getGame to DAO

--- app/src/main/java/com/example/basketballcounter/database/GameDao.kt
+++ app/src/main/java/com/example/basketballcounter/database/GameDao.kt
@@ -3,12 +3,16 @@ package com.example.basketballcounter.database
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.basketballcounter.Game
+import java.util.*
 
 @Dao
 interface GameDao {
 
     @Query("SELECT * FROM game")
     fun getGames(): List<Game>
+
+    @Query("SELECT * FROM game WHERE id=(:id)")
+    fun getGame(id: UUID) Game?
 
     @Query("SELECT * FROM game WHERE scoreA > scoreB")
     fun getAWins(): List<Game>
